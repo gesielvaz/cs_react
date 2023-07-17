81124ee9d00c3085835abe3720555ad2178eca0d
gesiel <gesieldosanjos@mobilezone.com.py>
2023-07-17 10:34:09 -0400

    fim do cursinho

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,3 +1,22 @@
-export function Footer(){
-    return <footer>Rodapé</footer>
+import styles from './Footer.module.css';
+import{FaFacebook,FaInstagram, FaLinkedinIn} from 'react-icons/fa'
+
+
+export function Footer() {
+    return (
+        <footer>
+            <ul className={styles.socialList}>
+                <li>
+                    <FaFacebook/>
+                </li>
+                <li>
+                    <FaInstagram/>
+                </li>
+                <li>
+                    <FaLinkedinIn/>
+                </li>
+            </ul>
+            <p>Rodapé</p>
+        </footer>
+    )
 }
